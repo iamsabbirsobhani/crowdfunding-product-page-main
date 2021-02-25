8356499071e769c2ebcf030c5a95c88e3e8a00e7
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-02-25 21:55:30 +0600

    Updated: Primevue css file retrives

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import '../src/assets/main.css'
 import Card from 'primevue/card';
 import Button from 'primevue/button';
 
-import 'primevue/resources/themes/saga-blue/theme.css'       //theme
-import 'primevue/resources/primevue.min.css'                 //core css
+import '@/resources/themes/saga-blue/theme.css'       //theme
+import '@/resources/primevue.min.css'                 //core css
 import 'primeicons/primeicons.css'                           //icons
 
 const app = createApp(App)
